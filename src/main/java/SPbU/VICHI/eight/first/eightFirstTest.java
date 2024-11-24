6f78ebb0f544851f8f685df37aa4baa6f61e24d0
krivonosovTi <krivonosovTi@gmail.com>
2024-11-25 01:34:19 +0300

    fech:  добавил ветку для 8ого задания

---
+++ src/main/java/SPbU/VICHI/eight/first/eightFirstTest.java
@@ -0,0 +1,15 @@
+package SPbU.VICHI.eight.first;
+
+import SPbU.VICHI.Test;
+import SPbU.VICHI.matrix.AbstractMatrix;
+import SPbU.VICHI.matrix.SquareMatrix;
+import SPbU.VICHI.matrix.Vector;
+import SPbU.VICHI.seven.first.SystemLinearEquations;
+
+import java.io.PrintStream;
+import java.security.SecureRandom;
+
+public class eightFirstTest implements Test {
+    public void test (PrintStream printStream ) {
+    }
+}
